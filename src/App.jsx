eb77d2e194b8ee13712b0d83efb21cedc0b58239
邱律誠 <luchengchiu@qiuluchengdeMacBook-Air.local>
2024-08-05 08:20:30 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Box, Center, Heading } from "@chakra-ui/react";
+import Part1 from "./part1";
+export default function App() {
+  return (
+    <Box>
+      <Part1 />
+    </Box>
+  );
+}
---
+++ src/part1.jsx
@@ -0,0 +1,23 @@
+import { Box, Center, Heading, Image } from "@chakra-ui/react";
+import mysteryBox from "/mybox.png";
+
+export default function Part1() {
+  return (
+    <Box w="100dvw" h="300px" bg="">
+      <Center>
+        <Heading>What is Front-End?</Heading>
+        <Image src={mysteryBox} />
+      </Center>
+    </Box>
+  );
+}
+// new idea
+// using 便當  排版
+// https://www.flaticon.com/free-icon/reception-desk_5890976?term=counter&page=1&position=47&origin=search&related_id=5890976
+// https://www.flaticon.com/search?word=mystery
+// https://www.flaticon.com/free-icon/backbone_3547529?term=bones&page=1&position=55&origin=search&related_id=3547529
+// https://www.flaticon.com/free-icon/laundry_2954918?term=cloth&page=1&position=2&origin=search&related_id=2954918
+// https://www.flaticon.com/free-icon/artificial-intelligence_8749970?term=circuit&page=1&position=70&origin=search&related_id=8749970
+// https://www.realtimecolors.com/?colors=030b16-edf3fd-287de2-9289ef-7562ea&fonts=Inter-Inter
+// https://www.figma.com/board/NEk6zgSTHzqIYtvggtOXcH/Untitled?node-id=0-1&t=qZrrJKlFfAm7tFZ1-1
+// bento
